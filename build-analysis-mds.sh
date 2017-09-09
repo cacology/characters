65d35546470e6ec734ed8068b791ce30c3bb0ad4
James P. Ascher <jpa4q@virginia.edu>
2017-09-09 15:29:35 -0400

    fixed xarg to long version

--- build-analysis-mds.sh
+++ build-analysis-mds.sh
@@ -3,4 +3,4 @@
 OUTFILE=${1#analysis/}
 OUTFILE=${OUTFILE%/}
 
-find $1 -name '*.md' -print0 | sort -z | xargs -0 -I {} cat {} lib/pagebreak.md > build/analysis-mds/$OUTFILE.md
+find $1 -name '*.md' -print0 | sort -z | xargs -0 -J {} cat {} lib/pagebreak.md > build/analysis-mds/$OUTFILE.md
--- build-text-mds.sh
+++ build-text-mds.sh
@@ -3,4 +3,4 @@
 OUTFILE=${1#text/}
 OUTFILE=${OUTFILE%/}
 
-find $1 -name '*.md' -print0 | sort -z | xargs -0 -I {} cat {} lib/pagebreak.md > build/text-mds/$OUTFILE.md
+find $1 -name '*.md' -print0 | sort -z | xargs -0 -J {} cat {} lib/pagebreak.md > build/text-mds/$OUTFILE.md
